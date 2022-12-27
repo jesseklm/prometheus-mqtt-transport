ab29de22577e8d3675f28b01b60b1ddd92651b99
Andreas Maus <maus@badphish.ypbind.de>
2022-12-27 18:42:30 +0100

    List TYPE and HELP only once
    TODO: for _count data for histograms, the data *must* folloow
    immediately after the histogram and summary data (type: histogram or summary). Otherwise
    promtool check metrics will complain

--- src/mqtt-export/data.rs
+++ src/mqtt-export/data.rs
@@ -1,5 +1,5 @@
 use flate2::bufread::GzDecoder;
-use log::debug;
+use log::{debug, error};
 use simple_error::bail;
 use std::collections::HashMap;
 use std::error::Error;
@@ -20,6 +20,10 @@ pub fn handler(
     let mut metrics_expiration: HashMap<String, i64> = HashMap::new();
     let mut now: i64;
     let mut expired: Vec<String> = Vec::new();
+    // HELP and TYPE strings *must* occur only once!
+    let mut metrics_type: HashMap<String, String> = HashMap::new();
+    let mut metrics_help: HashMap<String, String> = HashMap::new();
+    let mut metrics_data: HashMap<String, Vec<String>> = HashMap::new();
 
     loop {
         let request = data_receiver.recv()?;
@@ -38,7 +42,23 @@ pub fn handler(
                                 expired.push(name.to_string());
                             } else {
                                 for mtrc in mdata.payload.iter() {
-                                    reply.push(metric_to_string(mtrc));
+                                    metrics_type.insert(
+                                        mtrc.metric_name.to_string(),
+                                        mtrc.data_type.clone(),
+                                    );
+                                    metrics_help
+                                        .insert(mtrc.metric_name.to_string(), mtrc.help.clone());
+                                    let collected_data = metrics_data
+                                        .entry(mtrc.metric_name.to_string())
+                                        .or_default();
+                                    collected_data.extend(mtrc.data.clone());
+                                    debug!(
+                                        "collected type ({}), help ({}) and data ({}) for '{}'",
+                                        mtrc.data_type.clone(),
+                                        mtrc.help.clone(),
+                                        mtrc.data.clone().len(),
+                                        mtrc.metric_name
+                                    );
                                 }
                             }
                         } else {
@@ -50,7 +70,48 @@ pub fn handler(
                         }
                     }
                 }
+                for m in metrics_type.keys() {
+                    if let Some(mtype) = metrics_type.get(m) {
+                        if let Some(mhelp) = metrics_help.get(m) {
+                            if let Some(mdata) = metrics_data.get(m) {
+                                debug!(
+                                    "'{}' - TYPE: {} / HELP: {} / len(data) = {}",
+                                    m,
+                                    mtype,
+                                    mhelp,
+                                    mdata.len()
+                                );
+
+                                // TODO: for _count data for histograms, the data *must* folloow
+                                // immediately after the histogram and summary data (type: histogram or summary). Otherwise
+                                // promtool check metrics will complain
+                                let type_str = if !mtype.is_empty() {
+                                    format!("# TYPE {} {}", m, mtype)
+                                } else {
+                                    String::new()
+                                };
+                                let help_str = if !mhelp.is_empty() {
+                                    format!("# HELP {} {}", m, mhelp)
+                                } else {
+                                    String::new()
+                                };
+                                reply.push(format!(
+                                    "{}\n{}\n{}\n",
+                                    type_str,
+                                    mdata.join("\n"),
+                                    help_str
+                                ));
+                            }
+                        } else {
+                            error!("key {} found in metrics_type map but not in metrics_help, invalid metric format?", m);
+                        }
+                    }
+                }
                 http_reply.send(reply.join("\n"))?;
+
+                metrics_type.clear();
+                metrics_help.clear();
+                metrics_data.clear();
 
                 // remove expired data
                 for exp in expired.iter() {
@@ -70,17 +131,6 @@ pub fn handler(
             }
         };
     }
-}
-
-pub fn metric_to_string(payload: &global::payload::Payload) -> String {
-    format!(
-        "# TYPE {} {}\n{}\n# HELP {} {}",
-        payload.metric_name,
-        payload.data_type,
-        payload.data.join("\n"),
-        payload.metric_name,
-        payload.help,
-    )
 }
 
 pub fn parse_raw_metrics(raw: Vec<u8>) -> Result<Vec<global::payload::Message>, Box<dyn Error>> {
